68d75c4d3d23ab2cf089ad8f5af8363e4c6bf46d
Sergio Gonzalez <swgonzalez@unbosque.edu.co>
2026-03-19 15:07:23 -0500

    fmt

--- front_webapp.tf
+++ front_webapp.tf
@@ -34,13 +34,13 @@ resource "azurerm_linux_web_app" "webappfront" {
 }
 
 resource "azurerm_key_vault" "keyvaultfrontmain" {
-  name                        = "main${local.name_suffix}"
-  location                    = azurerm_resource_group.rgfront.location
-  resource_group_name         = azurerm_resource_group.rgfront.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "standard"
-  rbac_authorization_enabled  = true
-  tags                        = local.tags
+  name                       = "main${local.name_suffix}"
+  location                   = azurerm_resource_group.rgfront.location
+  resource_group_name        = azurerm_resource_group.rgfront.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  rbac_authorization_enabled = true
+  tags                       = local.tags
 
   # access_policy {
   #   tenant_id = data.azuread_client_config.current.tenant_id
@@ -72,40 +72,40 @@ resource "azurerm_role_assignment" "roleassignemntkeyvaultfrontmain" {
   scope                = azurerm_key_vault.keyvaultfrontmain.id
   role_definition_name = "Key Vault Administrator"
   principal_id         = data.azurerm_client_config.current.object_id
-  depends_on = [ azurerm_key_vault.keyvaultfrontmain ]
+  depends_on           = [azurerm_key_vault.keyvaultfrontmain]
 }
 
 resource "azurerm_role_assignment" "roleassignemntkeyvaultfrontmain_sp" {
   scope                = azurerm_key_vault.keyvaultfrontmain.id
   role_definition_name = "Key Vault Administrator"
   principal_id         = azuread_service_principal.serviceprincipalfront.object_id
-  depends_on = [ azurerm_key_vault.keyvaultfrontmain ]
+  depends_on           = [azurerm_key_vault.keyvaultfrontmain]
 }
 
 resource "azurerm_key_vault_secret" "secretfrontmainVITE_LOGIN_URL" {
   name         = "VITE-LOGIN-URL"
   value        = "https://${module.Main_Login.front_azurerm_linux_web_app_hostname}"
   key_vault_id = azurerm_key_vault.keyvaultfrontmain.id
-  depends_on = [ azurerm_role_assignment.roleassignemntkeyvaultfrontmain_sp ]
+  depends_on   = [azurerm_role_assignment.roleassignemntkeyvaultfrontmain_sp]
 }
 
 resource "azurerm_key_vault_secret" "secretfrontmainVITE_WHATSAPP_URL" {
   name         = "VITE-WHATSAPP-URL"
   value        = "https://wa.me/573001112233?text=Hola BilAI%2C quiero conocer la plataforma"
   key_vault_id = azurerm_key_vault.keyvaultfrontmain.id
-  depends_on = [ azurerm_role_assignment.roleassignemntkeyvaultfrontmain_sp ]
+  depends_on   = [azurerm_role_assignment.roleassignemntkeyvaultfrontmain_sp]
 }
 
 resource "azurerm_key_vault_secret" "secretfrontmainVITE_CONTACT_EMAIL" {
   name         = "VITE-CONTACT-EMAIL"
   value        = "ventas@bilai.com"
   key_vault_id = azurerm_key_vault.keyvaultfrontmain.id
-  depends_on = [ azurerm_role_assignment.roleassignemntkeyvaultfrontmain_sp ]
+  depends_on   = [azurerm_role_assignment.roleassignemntkeyvaultfrontmain_sp]
 }
 
 resource "azurerm_key_vault_secret" "secretfrontmainVITE_GA_MEASUREMENT_ID" {
   name         = "VITE-GA-MEASUREMENT-ID"
   value        = "G-XXXXXXXXXX"
   key_vault_id = azurerm_key_vault.keyvaultfrontmain.id
-  depends_on = [ azurerm_role_assignment.roleassignemntkeyvaultfrontmain_sp ]
+  depends_on   = [azurerm_role_assignment.roleassignemntkeyvaultfrontmain_sp]
 }
